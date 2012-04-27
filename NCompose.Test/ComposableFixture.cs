4b149b2bd597392eedeae02d56e022d05ca7e59d
anthony.taranto <anthony.taranto@rd.io>
2012-04-27 01:15:29 -0700

    add unit tests for more comprehensive set of method signatures

--- NCompose.Test/ComposableFixture.cs
+++ NCompose.Test/ComposableFixture.cs
@@ -1,15 +1,15 @@
-﻿using Xunit;
-using System;
+﻿using System;
+using Xunit;
 
 namespace NCompose.Test
 {
-    public interface ITest
+    public interface ISimpleInterface
     {
         bool Method1();
         bool Method2();
     }
 
-    internal class Class1
+    internal class SimpleClass1
     {
         public bool Method1()
         {
@@ -17,7 +17,7 @@ namespace NCompose.Test
         }
     }
 
-    internal class Class2
+    internal class SimpleClass2
     {
         public bool Method2()
         {
@@ -25,15 +25,54 @@ namespace NCompose.Test
         }
     }
 
+    public interface ICompleteInterface
+    {
+        int Property { get; set; }
+        void Out(out int x);
+        void Ref(ref int x);
+        T Generic<T>();
+    }
+
+    internal class CompleteClass : ICompleteInterface
+    {
+        public int Property
+        {
+            get
+            {
+                return 1;
+            }
+
+            set
+            {
+                
+            }
+        }
+
+        public void Out(out int x)
+        {
+            x = 1;
+        }
+
+        public void Ref(ref int x)
+        {
+            x = 1;
+        }
+
+        public T Generic<T>()
+        {
+            return default(T);
+        }
+    }
+
     public class ComposableFixture
     {
         [Fact]
-        public void TestBasicCreateSyntax()
+        private void TestBasicCreateSyntax()
         {
-            var test = Composable.Create<ITest>(composable =>
+            var test = Composable.Create<ISimpleInterface>(composable =>
             {
-                composable.AddPart(new Class1());
-                composable.AddPart(new Class2());
+                composable.AddPart(new SimpleClass1());
+                composable.AddPart(new SimpleClass2());
             });
 
             Assert.True(test.Method1());
@@ -43,34 +82,85 @@ namespace NCompose.Test
         ////
 
         [Fact]
-        public void ComposableImplementsInterface()
+        private void ComposableImplementsInterface()
         {
-            var composable = Composable.Create<ITest>();
+            var composable = Composable.Create<ISimpleInterface>();
             Assert.IsAssignableFrom<IComposable>(composable);
         }
 
         [Fact]
-        public void AddsPart()
+        private void AddsPart()
         {
-            Composable.Create<ITest>(composable =>
+            Composable.Create<ISimpleInterface>(composable =>
             {
-                composable.AddPart(new Class1());
+                composable.AddPart(new SimpleClass1());
             });
         }
 
         [Fact]
-        public void LooseBehaviorReturnsDefault()
+        private void LooseBehaviorReturnsDefault()
         {
-            var test = Composable.Create<ITest>(CompositionBehavior.Loose);
+            var test = Composable.Create<ISimpleInterface>(CompositionBehavior.Loose);
             Assert.Equal(default(bool), test.Method1());
             Assert.Equal(default(bool), test.Method2());
         }
 
         [Fact]
-        public void StrictBehaviorThrowsException()
+        private void StrictBehaviorThrowsException()
         {
-            var test = Composable.Create<ITest>(CompositionBehavior.Strict);
+            var test = Composable.Create<ISimpleInterface>(CompositionBehavior.Strict);
             Assert.Throws<InvalidOperationException>(() => test.Method1());
+        }
+
+        ////
+
+        [Fact]
+        private void CallsGetProperty()
+        {
+            var test = GetCompleteComposable();
+            Assert.Equal(1, test.Property);
+        }
+
+        [Fact]
+        private void CallsSetProperty()
+        {
+            var test = GetCompleteComposable();
+            test.Property = 1;
+        }
+
+        [Fact]
+        private void CallsOutMethod()
+        {
+            var test = GetCompleteComposable();
+
+            int x;
+            test.Out(out x);
+            Assert.Equal(1, x);
+        }
+
+        [Fact]
+        private void CallsRefMethod()
+        {
+            var test = GetCompleteComposable();
+
+            int x = 0;
+            test.Ref(ref x);
+            Assert.Equal(1, x);
+        }
+
+        [Fact]
+        private void CallsGenericMethod()
+        {
+            var test = GetCompleteComposable();
+            Assert.Equal(default(int), test.Generic<int>());
+        }
+
+        private static ICompleteInterface GetCompleteComposable()
+        {
+            return Composable.Create<ICompleteInterface>(CompositionBehavior.Strict, composable =>
+            {
+                composable.AddPart(new CompleteClass());
+            });
         }
     }
 }
