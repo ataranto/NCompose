43cd4980f29fbe6f9dc1961d1d44631c4f2acb20
anthony.taranto <anthony.taranto@rd.io>
2012-08-05 22:08:09 -0700

    move Aggregate class into Aggregate.cs

--- NCompose/Composable.cs
+++ NCompose/Composable.cs
@@ -5,31 +5,6 @@ using Castle.DynamicProxy;
 
 namespace NCompose
 {
-    internal abstract class Aggregate
-    {
-        public object Func(object a, object b)
-        {
-            return InternalFunc(a, b);
-        }
-
-        protected abstract object InternalFunc(object a, object b);
-    }
-
-    internal class Aggregate<T> : Aggregate
-    {
-        private readonly Func<T, T, T> func;
-
-        public Aggregate(Func<T, T, T> func)
-        {
-            this.func = func;
-        }
-
-        protected override object InternalFunc(object a, object b)
-        {
-            return this.func((T)a, (T)b);
-        }
-    }
-
     public class Composable : IInterceptor, IComposable
     {
         private readonly CompositionBehavior behavior;
